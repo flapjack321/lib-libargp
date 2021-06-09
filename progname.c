af1bd74c7b1952c077a0a0c493eed47a5feea84d
Ethan Cotterell <e.cotterell@lancaster.ac.uk>
2021-06-09 11:58:38 +0000

    src: update progname headers

--- progname.c
+++ progname.c
@@ -14,7 +14,7 @@
 #include <config.h>
 /* Specification.  */
 #undef ENABLE_RELOCATABLE /* avoid defining set_program_name as a macro */
-#include "progname.h"
+#include <progname.h>
 #include <errno.h> /* get program_invocation_name declaration */
 #include <stdio.h>
 #include <stdlib.h>
